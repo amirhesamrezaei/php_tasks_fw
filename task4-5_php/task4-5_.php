1c9a91f12a2b3cd4f901b4d108c227d057662c65
amirhesamrezaei <car86477@gmail.com>
2024-06-17 11:23:08 +0430

    edit and plus oop in project

--- task4-5_php/task4-5_.php
+++ task4-5_php/task4-5_.php
@@ -46,10 +46,94 @@ class students {
     }
 
 
+}
+
+
+$student = new students($_POST['fname'], $_POST['lname']);
+
+
+
+var_dump($_POST);
+echo "<hr>";
 
 
 
 
+
+if(!empty($_POST['stdid'])){
+
+    if (is_numeric($_POST['stdid'])) {
+
+        echo "<br>";
+        $student -> set_id($_POST['stdid']);
+        echo $_POST['stdid'];
+    
+    }
+    elseif (!is_numeric($_POST['stdid'])) {
+        
+       echo "attention to wrong format";
+       
+    }
+
+}
+else{
+
+    echo "attention to fild empty";
+
+
+}
+
+echo "<br>";
+
+
+if(!empty($_POST['stdmeli'])){
+
+    if (is_numeric($_POST['stdmeli'])) {
+
+        echo "<br>";
+        $student -> set_melliId($_POST['stdmeli']);
+        echo $_POST['stdmeli'];
+    
+    }
+    elseif (!is_numeric($_POST['stdmeli'])) {
+        
+       echo "attention to wrong format";
+       
+    }
+
+}
+else{
+
+    echo "attention to fild empty";
+
+
+}
+
+
+
+echo "<br>";
+
+
+
+if(!empty($_POST['fname'])){
+
+    if (!is_numeric($_POST['fname'])) {
+
+        echo "<br>";
+        $student -> set_fname($_POST['fname']);
+        echo $_POST['fname'];
+    
+    }
+    elseif (is_numeric($_POST['fname'])) {
+        
+       echo "attention to wrong format";
+       
+    }
+
+}
+else{
+
+    echo "attention to fild empty";
 
 
 }
@@ -57,136 +141,51 @@ class students {
 
 
 
-
-// var_dump($_POST);
-// echo "<hr>";
+echo "<br>";
 
 
 
+if(!empty($_POST['lname'])){
 
+    if (!is_numeric($_POST['lname'])) {
 
-// if(!empty($_POST['stdid'])){
-
-//     if (is_numeric($_POST['stdid'])) {
-
-//         echo "<br>";
-//         echo $_POST['stdid'];
+        echo "<br>";
+        $student -> set_lname($_POST['lname']);
+        echo $_POST['lname'];
     
-//     }
-//     elseif (!is_numeric($_POST['stdid'])) {
+    }
+    elseif (is_numeric($_POST['lname'])) {
         
-//        echo "attention to wrong format";
+       echo "attention to wrong format";
        
-//     }
+    }
 
-// }
-// else{
+}
+else{
 
-//     echo "attention to fild empty";
-
-
-// }
-
-// echo "<br>";
+    echo "attention to fild empty";
 
 
-// if(!empty($_POST['stdmeli'])){
-
-//     if (is_numeric($_POST['stdmeli'])) {
-
-//         echo "<br>";
-//         echo $_POST['stdmeli'];
-    
-//     }
-//     elseif (!is_numeric($_POST['stdmeli'])) {
-        
-//        echo "attention to wrong format";
-       
-//     }
-
-// }
-// else{
-
-//     echo "attention to fild empty";
+}
 
 
-// }
+//w
+
+$myfile = fopen("information.txt", "a") or die("Unable to open file!");
+$txt = $_POST['fname'] . "\n" ;
+fwrite($myfile, $txt);
 
 
+$txt = $_POST['lname'] . "\n" ;
+fwrite($myfile, $txt);
 
-// echo "<br>";
+$txt = $_POST['stdmeli'] . "\n" ;
+fwrite($myfile, $txt);
 
+$txt = $_POST['stdid'] . "\n" ;
+fwrite($myfile, $txt);
 
-
-// if(!empty($_POST['fname'])){
-
-//     if (!is_numeric($_POST['fname'])) {
-
-//         echo "<br>";
-//         echo $_POST['fname'];
-    
-//     }
-//     elseif (is_numeric($_POST['fname'])) {
-        
-//        echo "attention to wrong format";
-       
-//     }
-
-// }
-// else{
-
-//     echo "attention to fild empty";
-
-
-// }
-
-
-
-
-// echo "<br>";
-
-
-
-// if(!empty($_POST['lname'])){
-
-//     if (!is_numeric($_POST['lname'])) {
-
-//         echo "<br>";
-//         echo $_POST['lname'];
-    
-//     }
-//     elseif (is_numeric($_POST['lname'])) {
-        
-//        echo "attention to wrong format";
-       
-//     }
-
-// }
-// else{
-
-//     echo "attention to fild empty";
-
-
-// }
-
-
-// //w
-
-// $myfile = fopen("information.txt", "a") or die("Unable to open file!");
-// $txt = $_POST['fname'] . "\n" ;
-// fwrite($myfile, $txt);
-
-
-// $txt = $_POST['lname'] . "\n" ;
-// fwrite($myfile, $txt);
-
-// $txt = $_POST['stdmeli'] . "\n" ;
-// fwrite($myfile, $txt);
-
-// $txt = $_POST['stdid'] . "\n" ;
-// fwrite($myfile, $txt);
-
-// fclose($myfile);
+fclose($myfile);
 
 
 
