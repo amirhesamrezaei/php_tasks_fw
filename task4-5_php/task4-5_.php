68f9f9d6d149f6e37e2f33b381300aa02842052a
amirhesamrezaei <car86477@gmail.com>
2024-06-17 10:55:05 +0430

    plus oop to task

--- task4-5_php/task4-5_.php
+++ task4-5_php/task4-5_.php
@@ -1,134 +1,139 @@
 <?php
 
-var_dump($_POST);
-echo "<hr>";
 
 
 
 
 
-if(!empty($_POST['stdid'])){
+// var_dump($_POST);
+// echo "<hr>";
 
-    if (is_numeric($_POST['stdid'])) {
 
-        echo "<br>";
-        echo $_POST['stdid'];
+
+
+
+// if(!empty($_POST['stdid'])){
+
+//     if (is_numeric($_POST['stdid'])) {
+
+//         echo "<br>";
+//         echo $_POST['stdid'];
     
-    }
-    elseif (!is_numeric($_POST['stdid'])) {
+//     }
+//     elseif (!is_numeric($_POST['stdid'])) {
         
-       echo "attention to wrong format";
+//        echo "attention to wrong format";
        
-    }
+//     }
 
-}
-else{
+// }
+// else{
 
-    echo "attention to fild empty";
-
-
-}
-
-echo "<br>";
+//     echo "attention to fild empty";
 
 
-if(!empty($_POST['stdmeli'])){
+// }
 
-    if (is_numeric($_POST['stdmeli'])) {
+// echo "<br>";
 
-        echo "<br>";
-        echo $_POST['stdmeli'];
+
+// if(!empty($_POST['stdmeli'])){
+
+//     if (is_numeric($_POST['stdmeli'])) {
+
+//         echo "<br>";
+//         echo $_POST['stdmeli'];
     
-    }
-    elseif (!is_numeric($_POST['stdmeli'])) {
+//     }
+//     elseif (!is_numeric($_POST['stdmeli'])) {
         
-       echo "attention to wrong format";
+//        echo "attention to wrong format";
        
-    }
+//     }
 
-}
-else{
+// }
+// else{
 
-    echo "attention to fild empty";
-
-
-}
+//     echo "attention to fild empty";
 
 
-
-echo "<br>";
+// }
 
 
 
-if(!empty($_POST['fname'])){
+// echo "<br>";
 
-    if (!is_numeric($_POST['fname'])) {
 
-        echo "<br>";
-        echo $_POST['fname'];
+
+// if(!empty($_POST['fname'])){
+
+//     if (!is_numeric($_POST['fname'])) {
+
+//         echo "<br>";
+//         echo $_POST['fname'];
     
-    }
-    elseif (is_numeric($_POST['fname'])) {
+//     }
+//     elseif (is_numeric($_POST['fname'])) {
         
-       echo "attention to wrong format";
+//        echo "attention to wrong format";
        
-    }
+//     }
 
-}
-else{
+// }
+// else{
 
-    echo "attention to fild empty";
-
-
-}
+//     echo "attention to fild empty";
 
 
-
-
-echo "<br>";
+// }
 
 
 
-if(!empty($_POST['lname'])){
 
-    if (!is_numeric($_POST['lname'])) {
+// echo "<br>";
 
-        echo "<br>";
-        echo $_POST['lname'];
+
+
+// if(!empty($_POST['lname'])){
+
+//     if (!is_numeric($_POST['lname'])) {
+
+//         echo "<br>";
+//         echo $_POST['lname'];
     
-    }
-    elseif (is_numeric($_POST['lname'])) {
+//     }
+//     elseif (is_numeric($_POST['lname'])) {
         
-       echo "attention to wrong format";
+//        echo "attention to wrong format";
        
-    }
+//     }
 
-}
-else{
+// }
+// else{
 
-    echo "attention to fild empty";
-
-
-}
+//     echo "attention to fild empty";
 
 
-//w
-
-$myfile = fopen("information.txt", "a") or die("Unable to open file!");
-$txt = $_POST['fname'] . "\n" ;
-fwrite($myfile, $txt);
+// }
 
 
-$txt = $_POST['lname'] . "\n" ;
-fwrite($myfile, $txt);
+// //w
 
-$txt = $_POST['stdmeli'] . "\n" ;
-fwrite($myfile, $txt);
+// $myfile = fopen("information.txt", "a") or die("Unable to open file!");
+// $txt = $_POST['fname'] . "\n" ;
+// fwrite($myfile, $txt);
 
-$txt = $_POST['stdid'] . "\n" ;
-fwrite($myfile, $txt);
 
-fclose($myfile);
+// $txt = $_POST['lname'] . "\n" ;
+// fwrite($myfile, $txt);
+
+// $txt = $_POST['stdmeli'] . "\n" ;
+// fwrite($myfile, $txt);
+
+// $txt = $_POST['stdid'] . "\n" ;
+// fwrite($myfile, $txt);
+
+// fclose($myfile);
 
 
 
